9b17011630ab60e1d17a0d3d9a20b8ea8f56b094
dhornback10 <you@example.com>
2018-01-25 20:07:41 -0500

    Fixed the damages

--- Script.js
+++ Script.js
@@ -2,16 +2,20 @@ var grantHealth = 10;
 var playerHealth = 40;
 var playerWin = 0;
 var grantWin = 0;
-var Name = prompt("What is your name?");
+var question = prompt("Would you like to play the game?")
+if (question === "yes"){
+  var Name = prompt("What is your name?");
+}
 
 
 while (playerWin < 3 && grantWin != 1) {
-  var damage = Math.floor(Math.random() * 2) + 1;
+  var playerDamage = Math.floor(Math.random() * 2) + 1;
+  var grantDamage = Math.floor(Math.random() * 2) + 1;
   if (playerHealth >= 0 && grantHealth >= 0) {
-    playerHealth -= damage;
-    grantHealth -= damage;
-    console.log(Name + " has " + playerHealth + " left")
-    console.log("Grant has " + grantHealth + " left")
+    playerHealth -= playerDamage;
+    grantHealth -= grantDamage;
+    console.log(Name + " has " + playerHealth + " health left.")
+    console.log("Grant has " + grantHealth + " health left.")
   } else if (playerHealth <= 0) {
     grantWin++;
     console.log("Grant has won!");
@@ -23,4 +27,6 @@ while (playerWin < 3 && grantWin != 1) {
 }
 if (playerWin === 3) {
   console.log(Name + " has won!");
+} else if(grantWin===1){
+  console.log("Grant has won!")
 }
